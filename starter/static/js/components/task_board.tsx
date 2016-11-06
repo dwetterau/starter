6b7a59b628cd5c137fe4b43172c447afd94b14d6
David Wetterau <david.wetterau@gmail.com>
2016-11-05 17:38:54 -0700

    the UI part of drag and drop is mostly done

--- starter/static/js/components/task_board.tsx
+++ starter/static/js/components/task_board.tsx
@@ -1,4 +1,5 @@
 import * as React from "react";
+import * as jQuery from "jquery";
 import {Task, stateNameList} from "../models";
 import {TaskComponent} from "./task";
 
@@ -6,7 +7,9 @@ export interface TaskBoardProps {tasks: Array<Task>}
 export interface TaskBoardState {
     viewType: string,
     columns: Array<Array<Task>>,
-    headers: Array<string>
+    headers: Array<string>,
+    columnTypes: Array<number>,
+    draggingTask: Task
 }
 
 export const TaskBoardViewTypes = {
@@ -20,18 +23,23 @@ export class TaskBoardComponent extends React.Component<TaskBoardProps, TaskBoar
         super(props);
 
         const viewType = TaskBoardViewTypes.status;
-        const [headers, columns] = this.divideByType(this.props.tasks, viewType);
+        const [headers, columnTypes, columns] = this.divideByType(this.props.tasks, viewType);
         this.state = {
             viewType,
             columns,
-            headers
+            headers,
+            columnTypes,
+            draggingTask: null
         }
     }
 
-    divideByType(tasks: Array<Task>, type: string): [Array<string>, Array<Array<Task>>] {
+    divideByType(tasks: Array<Task>, type: string): [
+            Array<string>, Array<number>, Array<Array<Task>>] {
+
         const columns: {[columnType: number]: Array<Task>} = {};
         const columnList: Array<Array<Task>> = [];
         const headerList: Array<string> = [];
+        const columnTypes: Array<number> = [];
         if (type == TaskBoardViewTypes.status) {
             // Categorize each task
             tasks.forEach((task: Task) => {
@@ -47,18 +55,81 @@ export class TaskBoardComponent extends React.Component<TaskBoardProps, TaskBoar
                 let [state, value] = stateAndValue;
                 columnList.push(columns[value] || []);
                 headerList.push(state);
+                columnTypes.push(value);
             })
         } else {
             throw Error("Split type not implemented: " + type)
         }
-        return [headerList, columnList];
+        return [headerList, columnTypes, columnList];
     }
 
-    renderColumn(column: Array<Task>, header: string) {
-        return <div className="column-container" key={header}>
+    onDragStart(task: Task) {
+        if (this.state.draggingTask) {
+            throw Error("Already was dragging a task...")
+        }
+        console.log("started dragging task:", task);
+        this.state.draggingTask = task;
+        this.setState(this.state)
+    }
+
+    onDragEnd(task: Task) {
+        if (this.state.draggingTask != task) {
+            return
+        }
+
+        // Clean up any leftover state if we didn't successfully drop somewhere
+        this.state.draggingTask = null;
+        this.setState(this.state)
+    }
+
+    onDrop(columnType: number, event: DragEvent) {
+        if (!this.state.draggingTask) {
+            // No event was being dragged
+            return
+        }
+        event.preventDefault();
+
+        // Update the task with the new column
+        console.log("Would update task to column type: ", columnType);
+        console.log(this.state.draggingTask.state);
+
+        jQuery(event.target).removeClass("drop-container");
+
+        this.state.draggingTask = null;
+        this.setState(this.state)
+    }
+
+    onDragOver(event: any) {
+        if (!this.state.draggingTask) {
+            // No event was being dragged
+            return
+        }
+        event.preventDefault();
+        jQuery(event.target).addClass("drop-container")
+    }
+
+    onDragLeave(event: any) {
+        if (!this.state.draggingTask) {
+            // No event was being dragged
+            return
+        }
+        jQuery(event.target).removeClass("drop-container")
+    }
+
+    renderColumn(column: Array<Task>, header: string, columnType: number) {
+
+        return <div className="column-container" key={header}
+                    onDrop={this.onDrop.bind(this, columnType)}
+                    onDragOver={this.onDragOver.bind(this)}
+                    onDragLeave={this.onDragLeave.bind(this)} >
             <div className="column-header">{header}</div>
             {column.map((task) => {
-                return <TaskComponent task={task} viewType={this.state.viewType} key={task.id} />
+                // TODO: determine draggability programatically
+                return <div key={task.id} className="draggable-task"
+                            draggable={true} onDragStart={this.onDragStart.bind(this, task)}
+                            onDragEnd={this.onDragEnd.bind(this, task)} >
+                    <TaskComponent task={task} viewType={this.state.viewType} />
+                </div>
             })}
         </div>
     }
@@ -68,7 +139,11 @@ export class TaskBoardComponent extends React.Component<TaskBoardProps, TaskBoar
         let i = 0;
         for (; i < this.state.columns.length; i++) {
             renderedColumns.push(
-                this.renderColumn(this.state.columns[i], this.state.headers[i])
+                this.renderColumn(
+                    this.state.columns[i],
+                    this.state.headers[i],
+                    this.state.columnTypes[i],
+                )
             );
         }
         return <div className="full-column-container">
