ba28d5c390c61122c0a73c449e64ecdf44bc8b43
David Wetterau <david.wetterau@gmail.com>
2018-07-08 17:46:04 -0700

    added first jest tests for the capturer

---
+++ starter/static/js/components/__tests__/capturer.test.tsx
@@ -0,0 +1,41 @@
+import * as React from "react";
+import * as renderer from 'react-test-renderer';
+import {Capturer} from "../capturer";
+import {Capture, User} from "../../models";
+
+function mockUser(): User {
+   return {
+       id: 1,
+       firstName: "David",
+       lastName: "Wetterau",
+       email: "david@email.com",
+       username: "david",
+   }
+}
+
+test('Empty Capture List', () => {
+    const testRenderer = renderer.create(
+        <Capturer meUser={mockUser()} captures={[]}/>
+    );
+    let tree = testRenderer.toJSON();
+    expect(tree).toMatchSnapshot();
+});
+
+test('Multiple captures', () => {
+    let captures: Array<Capture> = [
+        {id: 1, content: "content 1", creationTime: 4, authorId: 1},
+        {id: 2, content: "content 2", creationTime: 5, authorId: 1},
+    ];
+    const testRenderer = renderer.create(
+        <Capturer meUser={mockUser()} captures={captures}/>
+    );
+    let tree = testRenderer.toJSON();
+    expect(tree).toMatchSnapshot();
+
+    // Now delete a capture
+    let capturer = testRenderer.root.findByType(Capturer).instance;
+    capturer.updateStateWithCaptures(captures.slice(1));
+
+    tree = testRenderer.toJSON();
+    expect(tree).toMatchSnapshot();
+});
